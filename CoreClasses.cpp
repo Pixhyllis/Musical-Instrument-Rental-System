75ac81d644ce372ad182b64384cee20d0ee90e17
chang9297985177-cloud <chang9297985177@gmail.com>
2026-06-22 12:51:03 +0000

    Added CoreClasses.cpp with empty function implementations

---
+++ CoreClasses.cpp
@@ -0,0 +1,73 @@
+#include "CoreClasses.h"
+
+// Instrument
+
+Instrument::Instrument(
+string name,
+string brand,
+string model,
+string instrumentID,
+double rentPerDay,
+bool isAvailable
+)
+{
+}
+
+Instrument::~Instrument()
+{
+}
+
+string Instrument::getName()
+{
+return "";
+}
+
+string Instrument::getBrand()
+{
+return "";
+}
+
+string Instrument::getModel()
+{
+return "";
+}
+
+string Instrument::getInstrumentID()
+{
+return "";
+}
+
+double Instrument::getRentPerDay()
+{
+return 0;
+}
+
+bool Instrument::getIsAvailable()
+{
+return false;
+}
+
+void Instrument::setName(string name)
+{
+}
+
+void Instrument::setBrand(string brand)
+{
+}
+
+void Instrument::setModel(string model)
+{
+}
+
+void Instrument::setInstrumentID(string instrumentID)
+{
+}
+
+void Instrument::setRentPerDay(double rentPerDay)
+{
+}
+
+void Instrument::setIsAvailable(bool isAvailable)
+{
+}
+
