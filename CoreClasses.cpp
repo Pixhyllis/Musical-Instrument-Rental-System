8080ee9d9be9bf4e4ec026463ffe0a2eb22c68d5
chang9297985177-cloud <chang9297985177@gmail.com>
2026-06-22 13:35:55 +0000

    Added CoreClasses.cpp with empty function implementations

--- CoreClasses.cpp
+++ CoreClasses.cpp
@@ -70,4 +70,116 @@ void Instrument::setRentPerDay(double rentPerDay)
 void Instrument::setIsAvailable(bool isAvailable)
 {
 }
+// customer is always right
+Customer::Customer(
+string name,
+string customerID,
+string email,
+string contactNumber
+)
+{
+}
 
+Customer::~Customer()
+{
+}
+
+string Customer::getName()
+{
+return "";
+}
+
+string Customer::getCustomerID()
+{
+return "";
+}
+
+string Customer::getEmail()
+{
+return "";
+}
+
+string Customer::getContactNumber()
+{
+return "";
+}
+
+void Customer::setName(string name)
+{
+}
+
+void Customer::setCustomerID(string customerID)
+{
+}
+
+void Customer::setEmail(string email)
+{
+}
+
+void Customer::setContactNumber(string contactNumber)
+{
+}
+
+Rental::~Rental()
+{
+}
+
+int Rental::getRentalDays()
+{
+return 0;
+}
+
+double Rental::getTotalCost()
+{
+return 0;
+}
+
+string Rental::getInstrumentID()
+{
+return "";
+}
+
+string Rental::getRentalID()
+{
+return "";
+}
+
+string Rental::getCustomerID()
+{
+return "";
+}
+
+void Rental::setRentalDays(int rentalDays)
+{
+}
+
+void Rental::setTotalCost(double totalCost)
+{
+}
+
+void Rental::setInstrumentID(string instrumentID)
+{
+}
+
+void Rental::setRentalID(string rentalID)
+{
+}
+
+void Rental::setCustomerID(string customerID)
+{
+}
+
+// SystemManager
+
+void SystemManager::displayAvailableInstruments()
+{
+}
+
+void SystemManager::displayAllRentals()
+{
+}
+
+void SystemManager::rentInstrument(
+string customerID,
+string instrumentID,
+int rentalDays
